f1a9652b672250eb432bb285d6243b8d2ed2d87c
Derik Dembinski <derikdembinski13@outlook.com>
2025-08-07 19:53:52 -0300

    Update live.sh

--- live.sh
+++ live.sh
@@ -17,7 +17,7 @@ sleep 3
 # Inicia o FFmpeg com os headers e grava a playlist no formato HLS
 ffmpeg \
 -headers "referer: https://embed.embedtv.net/\r\nuser-agent: Mozilla/5.0" \
--i "https://aws.vidfox.cloud/discovery-channel/index.m3u8" \
+-i "https://aws.vidfox.cloud/globo-sp/index.m3u8" \
 -c:v copy -c:a copy \
 -f hls \
 -hls_time 4 \
@@ -31,6 +31,7 @@ sleep 5
 
 # Abre o link local no navegador padrão (VLC ou browser)
 termux-open-url "http://127.0.0.1:8080/playlist.m3u8"
+
 
 
 

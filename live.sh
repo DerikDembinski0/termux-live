b218a611dcb795dff83b99c79c5bb608dcbe1ac6
Derik Dembinski <derikdembinski13@outlook.com>
2025-08-11 20:45:32 -0300

    Update live.sh

--- live.sh
+++ live.sh
@@ -16,7 +16,7 @@ import http.server, urllib.request, urllib.parse, urllib.error
 from urllib.parse import urljoin, urlparse, parse_qs, quote, unquote
 
 PORT = 8080
-BASE_URL = 'https://cdn001.sytrano.cfd/C/chefe-de-guerra-2025/01-temporada/01/'
+BASE_URL = 'https://cdn001.wclouda.site/K/karate-kid-lendas-2025/stream/stream.m3u8'
 M3U8_NAME = 'stream.m3u8'
 HEADERS = {
     'Referer': 'https://www.weekseries.info/',
@@ -191,3 +191,4 @@ fi
 
 echo "🎬 Seletor de apps deve aparecer agora. Pressione Ctrl+C para encerrar quando quiser."
 wait $SERVER_PID
+

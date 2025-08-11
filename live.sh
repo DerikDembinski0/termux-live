3a55c14934df007f8c0daf0b6e20470042733c4e
Derik Dembinski <derikdembinski13@outlook.com>
2025-08-10 23:52:02 -0300

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffplay -probesize 5M -analyzeduration 5M -headers "referer: https://www.weekseries.info/\r\norigin: https://www.weekseries.info\r\nuser-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36" -sync video -framedrop -autoexit "https://cdn001.sytrano.cfd/C/chefe-de-guerra-2025/01-temporada/01/stream.m3u8"
+ffplay -headers "referer: https://www.weekseries.info/\r\norigin: https://www.weekseries.info\r\nuser-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36" -autoexit "https://cdn001.sytrano.cfd/C/chefe-de-guerra-2025/01-temporada/01/stream.m3u8"

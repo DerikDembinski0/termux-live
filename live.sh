83a1da0142cd891ec22fbaaa38c0b578675ebd4b
Derik Dembinski <derikdembinski13@outlook.com>
2025-08-10 23:32:02 -0300

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-# Player M3U8 - Qualidade Máxima - Sem Interface
-
-ffplay -headers "referer: https://www.weekseries.info/\r\norigin: https://www.weekseries.info\r\nuser-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36" -window_title "Chefe de Guerra - Episódio 01" -autoexit "https://cdn001.sytrano.cfd/C/chefe-de-guerra-2025/01-temporada/01/stream.m3u8"
+ffplay -probesize 5M -analyzeduration 5M -headers "referer: https://www.weekseries.info/\r\norigin: https://www.weekseries.info\r\nuser-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36" -sync video -framedrop -autoexit "https://cdn001.sytrano.cfd/C/chefe-de-guerra-2025/01-temporada/01/stream.m3u8"
